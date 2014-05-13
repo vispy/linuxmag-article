163c911bbd373ded5c454af6979abe75dc78cee1
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-05-13 16:00:38 +0200

    WIP: graph with antialiased lines. Seems like there's a bug with multiple programs in Vispy.

--- graph.py
+++ graph.py
@@ -14,10 +14,36 @@ g = nx.gnp_random_graph(n, 0.05)
 M = nx.adjacency_matrix(g)
 Ms = M.sum(axis=1)
 positions = nx.spring_layout(g)
-positions = np.array([positions[k] for k in sorted(positions.keys())])-.5
-edges = np.vstack(g.edges()).ravel().astype(np.uint32)
+positions = np.array([positions[k] for k in sorted(positions.keys())], dtype=np.float32)-.5
+edges = np.vstack(g.edges()).astype(np.uint32)
 color = np.random.rand(len(positions), 3)
 size = np.random.randint(low=3, high=20, size=n)
+
+
+start = positions[edges[:,0],:]
+end = positions[edges[:,1],:]
+
+# normal = end - start
+# normal = np.fliplr(normal)
+# normal[:,0] *= -1
+# normal /= np.sqrt((normal**2).sum(axis=1)).reshape((-1,1))
+
+# h = .00
+
+positions_edges = np.dstack((
+    start,# - h*normal,
+    end,# - h*normal,
+    start,# + h*normal,
+
+    end,# + h*normal,
+    start,# + h*normal,
+    end,# - h*normal,
+))#.transpose((2, 1, 0)).ravel().reshape((-1, 2))
+
+positions_edges = positions.copy()
+positions_edges[:]=0
+positions_edges = positions_edges.astype(np.float32)
+
 
 data = np.zeros(n, dtype=[
         ('a_position', np.float32, 2),
@@ -46,9 +72,9 @@ varying float v_antialias;
 
 void main (void) {
     v_radius = a_size;
-    v_linewidth = 1.0;
+    v_linewidth = 3.0;
     v_antialias = 1.0;
-    v_fg_color  = vec4(0.0,0.0,0.0,0.5);
+    v_fg_color  = vec4(0.0,0.0,0.0,1.0);
     v_bg_color  = vec4(a_color,    1.0);
     
     vec2 position_tr = u_scale * (a_position + u_pan);
@@ -84,8 +110,22 @@ void main()
 }
 """
 
-FRAG_SHADER_EDGES = """
 
+
+VERT_SHADER_EDGES = """
+attribute vec2 a_position;
+
+uniform vec2 u_pan;
+uniform vec2 u_scale;
+
+void main (void) {
+    vec2 position_tr = u_scale * (a_position + u_pan);
+    gl_Position = vec4(position_tr, 0.0, 1.0);
+    gl_PointSize = 5.;
+}
+"""
+
+FRAG_SHADER_EDGES = """
 void main()
 {
     gl_FragColor = vec4(0., 0., 0., 1.);
@@ -98,13 +138,10 @@ class Canvas(app.Canvas):
         app.Canvas.__init__(self, close_keys='escape')
 
         self.program = gloo.Program(VERT_SHADER, FRAG_SHADER)
-        self.program_edges = gloo.Program(VERT_SHADER, FRAG_SHADER_EDGES)
-
-        self.vertex_buffer = gloo.VertexBuffer(data)
-        self.index_buffer = gloo.IndexBuffer(edges)
+        self.program.bind(gloo.VertexBuffer(data))
         
-        self.program.bind(self.vertex_buffer)
-        self.program_edges.bind(self.vertex_buffer)
+        self.program_edges = gloo.Program(VERT_SHADER_EDGES, FRAG_SHADER_EDGES)
+        self.program_edges['a_position'] = gloo.VertexBuffer(positions_edges)
         
         for p in (self.program, self.program_edges):
             p['u_pan'] = (0., 0.)
@@ -146,7 +183,7 @@ class Canvas(app.Canvas):
 
     def on_paint(self, event):
         gloo.clear()
-        self.program_edges.draw('lines', indices=self.index_buffer)
+        self.program_edges.draw('points')
         self.program.draw('points')
 
 if __name__ == '__main__':
