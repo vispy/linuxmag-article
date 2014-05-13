2cfae626449a8d2739a5b74ad462d8a51cee1d3c
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-05-13 21:50:07 +0200

    Added antialiased lines to graph example.

--- graph.py
+++ graph.py
@@ -10,39 +10,45 @@ import networkx as nx
 
 n = 100
 # g = nx.watts_strogatz_graph(n, 3, 0.5)
-g = nx.gnp_random_graph(n, 0.05)
+g = nx.gnp_random_graph(n, 0.04)
 M = nx.adjacency_matrix(g)
 Ms = M.sum(axis=1)
 positions = nx.spring_layout(g)
 positions = np.array([positions[k] for k in sorted(positions.keys())], dtype=np.float32)-.5
 edges = np.vstack(g.edges()).astype(np.uint32)
 color = np.random.rand(len(positions), 3)
-size = np.random.randint(low=3, high=20, size=n)
+size = np.random.randint(low=3, high=10, size=n)
 
 
 start = positions[edges[:,0],:]
 end = positions[edges[:,1],:]
 
-# normal = end - start
-# normal = np.fliplr(normal)
-# normal[:,0] *= -1
-# normal /= np.sqrt((normal**2).sum(axis=1)).reshape((-1,1))
+normal = end - start
+normal = np.fliplr(normal)
+normal[:,0] *= -1
+normal /= np.sqrt((normal**2).sum(axis=1)).reshape((-1,1))
 
-# h = .00
+h = .02
 
 positions_edges = np.dstack((
-    start,# - h*normal,
-    end,# - h*normal,
-    start,# + h*normal,
+    start - h*normal,
+    end - h*normal,
+    start + h*normal,
 
-    end,# + h*normal,
-    start,# + h*normal,
-    end,# - h*normal,
-))#.transpose((2, 1, 0)).ravel().reshape((-1, 2))
+    end + h*normal,
+    start + h*normal,
+    end - h*normal,
+)).transpose((0, 2, 1)).ravel().reshape((-1, 2))
 
-positions_edges = positions.copy()
-positions_edges[:]=0
-positions_edges = positions_edges.astype(np.float32)
+index_edges = np.array([
+    [0., 0.],
+    [1., 0.],
+    [0., 1.],
+    [1., 1.],
+    [0., 1.],
+    [1., 0.],
+], dtype=np.float32)
+index_edges = np.tile(index_edges, (edges.shape[0], 1))
 
 
 data = np.zeros(n, dtype=[
@@ -72,14 +78,15 @@ varying float v_antialias;
 
 void main (void) {
     v_radius = a_size;
-    v_linewidth = 3.0;
+    v_linewidth = 1.0;
     v_antialias = 1.0;
     v_fg_color  = vec4(0.0,0.0,0.0,1.0);
     v_bg_color  = vec4(a_color,    1.0);
     
     vec2 position_tr = u_scale * (a_position + u_pan);
     gl_Position = vec4(position_tr, 0.0, 1.0);
-    gl_PointSize = 2.0*(v_radius + v_linewidth + 1.5*v_antialias);
+    float z = sqrt(pow(u_scale.x,2) + pow(u_scale.y,2));
+    gl_PointSize = 2.0*z*(v_radius + v_linewidth + 1.5*v_antialias);
 }
 """
 
@@ -114,6 +121,8 @@ void main()
 
 VERT_SHADER_EDGES = """
 attribute vec2 a_position;
+attribute vec2 a_index;
+varying vec2 v_index;
 
 uniform vec2 u_pan;
 uniform vec2 u_scale;
@@ -121,14 +130,30 @@ uniform vec2 u_scale;
 void main (void) {
     vec2 position_tr = u_scale * (a_position + u_pan);
     gl_Position = vec4(position_tr, 0.0, 1.0);
-    gl_PointSize = 5.;
+    v_index = a_index;
 }
 """
 
 FRAG_SHADER_EDGES = """
+varying vec2 v_index;
 void main()
 {
-    gl_FragColor = vec4(0., 0., 0., 1.);
+    vec4 v_fg_color = vec4(0., 0., 0., 1.);
+    vec4 v_bg_color = vec4(0., 0., 0., 0.);
+    
+    float r = 10*(v_index.y - .5);
+    float d = abs(r) - .1;
+    if( d < 0.0 )
+        gl_FragColor = v_fg_color;
+    else
+    {
+        float alpha = d;
+        alpha = exp(-alpha*alpha);
+        if (abs(r) > 5.)
+            gl_FragColor = vec4(v_fg_color.rgb, alpha*v_fg_color.a);
+        else
+            gl_FragColor = mix(v_bg_color, v_fg_color, alpha);
+    }
 }
 """
 
@@ -142,6 +167,7 @@ class Canvas(app.Canvas):
         
         self.program_edges = gloo.Program(VERT_SHADER_EDGES, FRAG_SHADER_EDGES)
         self.program_edges['a_position'] = gloo.VertexBuffer(positions_edges)
+        self.program_edges['a_index'] = gloo.VertexBuffer(index_edges)
         
         for p in (self.program, self.program_edges):
             p['u_pan'] = (0., 0.)
@@ -183,7 +209,7 @@ class Canvas(app.Canvas):
 
     def on_paint(self, event):
         gloo.clear()
-        self.program_edges.draw('points')
+        self.program_edges.draw('triangles')
         self.program.draw('points')
 
 if __name__ == '__main__':
